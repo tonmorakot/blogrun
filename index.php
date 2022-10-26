fded86859cb9146b8cf3496a35ac6c2e447b0ff4
TON MARAKATA <tonmorakot@gmail.com>
2022-10-26 21:34:00 +0700

    1

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="assets\node_modules\bootstrap\dist\css\bootstrap.min.css">
+
+  <title>Blog Run</title>
+</head>
+
+<body>
+
+
+  <h1>Test</h1>
+  <button class="btn btn-primary">Test</button>
+
+  <script src="assets\node_modules\bootstrap\dist\js\bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
