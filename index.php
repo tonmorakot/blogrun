6ad8dcb733fe90f75d9b5414cfdd0591484b8914
TON MARAKATA <tonmorakot@gmail.com>
2022-10-26 23:06:54 +0700

    3

--- index.php
+++ index.php
@@ -6,7 +6,107 @@ include("components/head.php");
 <body>
   <?php include("components/navbar.php"); ?>
 
-  <div class="blogrun">หน้าหลัก</div>
+  <div class="blogrun">
+    <div class="col-md-12">
+      <div class="row">
+
+        <div class="col-md-4 mt-3">
+
+          <div class="card">
+            <div class="card-header">
+              Lorem ipsum dolor sit amet.
+            </div>
+            <div class="card-body">
+              <blockquote class="blockquote mb-0">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo maxime in perspiciatis necessitatibus doloremque facilis numquam. Rerum vero quaerat aut?</p>
+              </blockquote>
+            </div>
+          </div>
+
+        </div>
+
+        <div class="col-md-4 mt-3">
+
+          <div class="card">
+            <div class="card-header">
+              Lorem ipsum dolor sit amet.
+            </div>
+            <div class="card-body">
+              <blockquote class="blockquote mb-0">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo maxime in perspiciatis necessitatibus doloremque facilis numquam. Rerum vero quaerat aut?</p>
+              </blockquote>
+            </div>
+          </div>
+
+        </div>
+
+        <div class="col-md-4 mt-3">
+
+          <div class="card">
+            <div class="card-header">
+              Lorem ipsum dolor sit amet.
+            </div>
+            <div class="card-body">
+              <blockquote class="blockquote mb-0">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo maxime in perspiciatis necessitatibus doloremque facilis numquam. Rerum vero quaerat aut?</p>
+              </blockquote>
+            </div>
+          </div>
+
+        </div>
+
+        <div class="col-md-4 mt-3">
+
+          <div class="card">
+            <div class="card-header">
+              Lorem ipsum dolor sit amet.
+            </div>
+            <div class="card-body">
+              <blockquote class="blockquote mb-0">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo maxime in perspiciatis necessitatibus doloremque facilis numquam. Rerum vero quaerat aut?</p>
+              </blockquote>
+            </div>
+          </div>
+
+        </div>
+
+        <div class="col-md-4 mt-3">
+
+          <div class="card">
+            <div class="card-header">
+              Lorem ipsum dolor sit amet.
+            </div>
+            <div class="card-body">
+              <blockquote class="blockquote mb-0">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo maxime in perspiciatis necessitatibus doloremque facilis numquam. Rerum vero quaerat aut?</p>
+              </blockquote>
+            </div>
+          </div>
+
+        </div>
+
+        <div class="col-md-4 mt-3">
+
+          <div class="card">
+            <div class="card-header">
+              Lorem ipsum dolor sit amet.
+            </div>
+            <div class="card-body">
+              <blockquote class="blockquote mb-0">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo maxime in perspiciatis necessitatibus doloremque facilis numquam. Rerum vero quaerat aut?</p>
+              </blockquote>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+
+
+
+    </div>
+  </div>
+
 
   <?php include("components/script.php"); ?>
 </body>
